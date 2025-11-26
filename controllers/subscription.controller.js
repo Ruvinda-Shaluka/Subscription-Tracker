39b09955ca244dc6972d703fc03accbb8c14b74d
Ruvinda24 <shalukaofficial24@gmail.com>
2025-11-26 14:45:54 +0530

    3rd commit on 26th November with re correcting error messages.

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -3,13 +3,12 @@ import Subscription from "../models/subscription.model.js";
 
 export const createSubscription = async (req, res, next) => {
     try {
-        const subscription = await Subscription.create(
-            {
-                ...req.body,
-                user: req.user._id
-            });
+        const subscription = await Subscription.create({
+            ...req.body,
+            user: req.user._id,
+        });
 
-        res.status(201).json({success: true, message: 'Subscription created', data: subscription});
+        res.status(201).json({ success: true, data: { subscription } });
     }catch(err) {
         next(err);
     }
--- middlewares/arcjet.middleware.js
+++ middlewares/arcjet.middleware.js
@@ -1,24 +1,20 @@
 import aj from '../config/arcjet.js';
 
-const  arcjetMiddleware = async (req, res, next) => {
+const arcjetMiddleware = async (req, res, next) => {
     try {
-        const decision = await aj.protect(req,{ requested: 1 });
+        const decision = await aj.protect(req, { requested: 1 });
 
-        if (decision.isDenied()) {
-            if (decision.reason.isRateLimit()) {
-                return res.status(429).json({error: decision.reason});
-            }
-            if (decision.reason.isBot()) {
-                return res.status(403).json({error: decision.reason});
-            }
+        if(decision.isDenied()) {
+            if(decision.reason.isRateLimit()) return res.status(429).json({ error: 'Rate limit exceeded' });
+            if(decision.reason.isBot()) return res.status(403).json({ error: 'Bot detected' });
 
-            return res.status(403).json({error: decision.reason});
+            return res.status(403).json({ error: 'Access denied' });
         }
 
         next();
-    } catch (err) {
-        console.log(`Arcjet Middleware Error: ${err.message}`);
-        next(err);
+    } catch (error) {
+        console.log(`Arcjet Middleware Error: ${error}`);
+        next(error);
     }
 }
 
